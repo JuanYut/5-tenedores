0498b61095c651c528ece724a528a7e325315e36
JuanYut <juanyut97@hotmail.com>
2020-08-17 18:36:00 -0500

    register with firebase done

---
+++ app/screens/Account/UserGuest.js
@@ -0,0 +1,64 @@
+import React from "react";
+import { StyleSheet, View, ScrollView, Text, Image } from "react-native";
+import { Button } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const styles = StyleSheet.create({
+  viewBody: {
+    marginLeft: 30,
+    marginRight: 30,
+  },
+  image: {
+    height: 300,
+    width: "100%",
+    marginBottom: 40,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 19,
+    marginBottom: 10,
+    textAlign: "center",
+  },
+  description: {
+    textAlign: "center",
+    marginBottom: 20,
+  },
+  viewButton: {
+    flex: 1,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    width: "70%",
+  },
+  button: {
+    backgroundColor: "#00a680",
+  },
+});
+
+export default function UserGuest() {
+  const navigation = useNavigation();
+  console.log(navigation);
+
+  return (
+    <ScrollView centerContent={true} style={styles.viewBody}>
+      <Image
+        style={styles.image}
+        source={require("../../../assets/img/user-guest.jpg")}
+      />
+      <Text style={styles.title}>Consulta tu perfil de 5 Tenedores</Text>
+      <Text style={styles.description}>
+        ¿Como describirías tu mejor restaurante? Busca y visualiza los mejores
+        restaurantes de una forma sencilla, vota cual te ha gustado más y
+        comenta como ha sito tu experiencia.
+      </Text>
+      <View style={styles.viewButton}>
+        <Button
+          buttonStyle={styles.button}
+          containerStyle={styles.buttonContainer}
+          title="Ver tu perfil"
+          onPress={() => navigation.navigate("login")}
+        />
+      </View>
+    </ScrollView>
+  );
+}
